36b4e3cf197a06d948abcd4cf6728d7ba5e89c90
Ozy <github@oselcuk.com>
2024-12-08 10:23:27 +0100

    Day 8 part 1

---
+++ day08/solve.1.ex
@@ -0,0 +1,37 @@
+defmodule Solution do
+  @spec solve(list(String.t())) :: integer()
+  def solve(matrix) do
+    nodes =
+      for {row, i} <- Enum.with_index(matrix),
+          {c, j} <- Enum.with_index(String.to_charlist(row)),
+          c != ?.,
+          reduce: Map.new() do
+        acc -> Map.update(acc, c, [{i, j}], &[{i, j} | &1])
+      end
+
+    # Assume square matrix
+    dim = length(matrix)
+    valid = fn {x, y} -> x >= 0 and x < dim and y >= 0 and y < dim end
+
+    antinodes = fn {x1, y1}, {x2, y2} ->
+      dx = x1 - x2
+      dy = y1 - y2
+      [{x1 + dx, y1 + dy}, {x2 - dx, y2 - dy}]
+    end
+
+    for {_, locations} <- nodes,
+        a <- locations,
+        b <- locations,
+        a != b,
+        antinode <- antinodes.(a, b),
+        valid.(antinode),
+        uniq: true,
+        do: antinode
+  end
+end
+
+IO.read(:eof)
+|> String.split("\n", trim: true)
+|> Solution.solve()
+|> Enum.count()
+|> IO.inspect()
